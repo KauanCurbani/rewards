f94ec9ef091f9bab9ccd6fb8bac222a84f10f9ec
Kauan Curbani <crbnii2020@gmail.com>
2025-08-28 16:30:07 -0300

    fix: comment out loader screen condition for worker readiness

--- src/app/(theme)/layout.tsx
+++ src/app/(theme)/layout.tsx
@@ -19,7 +19,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     f();
   }, []);
 
-  if (!workerReady) return <LoaderScreen />;
+  // if (!workerReady) return <LoaderScreen />;
 
   return (
     <AppThemeProvider>
